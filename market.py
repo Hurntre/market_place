92f41f81c95e213636c1babf217ff05052ecdedb
Adefolaju <adefolajuariyo@gmail.com>
2024-07-19 11:48:41 +0100

    Set up SQLite database connection using the SQLALCHEMY ORM package
    Create item model
    
    Use template extension to keep template dry

--- market.py
+++ market.py
@@ -1,12 +1,30 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
+from database import init_db, db_session
+from models import Item
+init_db()
 
-@app.route("/") #decorator 
-@app.route("/home")
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+# item = Item('phone', 500, '893212299897', 'A UK used Iphone 30')
+# db_session.add(item)
+# db_session.commit()
+
+
+@app.route('/') #decorator 
+@app.route('/home')
 def home_page():
   return render_template('home.html')
 
-@app.route("/about/<username>")
+@app.route('/market')
+def market_page():
+
+  items = Item.query.all()
+  return render_template('market.html', items=items)
+
+@app.route('/about/<username>')
 def about_page(username):
-    return f"<h1>This is the about page of {username}</h1>"
+    return f'<h1>This is the about page of {username}</h1>'
---
+++ models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(30), nullable=False, unique=True)
+    price = Column(Integer(), nullable=False)
+    barcode = Column(String(12), nullable=False, unique=True)
+    description = Column(String(1024), nullable=False, unique=True)
+
+    def __init__(self, name, price, barcode, description):
+        self.name = name
+        self.price = price
+        self.barcode = barcode
+        self.description = description
+
+    def __repr__(self):
+        return f'<Item{self.id} {self.name} {self.price} {self.barcode} {self.description}>'
