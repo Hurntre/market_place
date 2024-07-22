a9f05a3c0136e9d932317beb5dd578212912cbb2
Adefolaju <adefolajuariyo@gmail.com>
2024-07-22 13:33:08 +0100

    add user log out functionality

--- market/__init__.py
+++ market/__init__.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '11c23e85b73743e4b6c7b14a'
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = 'info'
 
 engine = create_engine("sqlite+pysqlite:///market/market.db", echo=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
--- market/routes.py
+++ market/routes.py
@@ -2,7 +2,7 @@ from market import app, db_session
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route('/')
@@ -11,6 +11,7 @@ def home_page():
   return render_template('home.html')
 
 @app.route('/market')
+@login_required 
 def market_page():
 
   items = Item.query.all()
@@ -24,6 +25,9 @@ def register_page():
     user_to_create = User(form.username.data, form.email_address.data, form.password1.data)
     db_session.add(user_to_create)
     db_session.commit()
+
+    login_user(user_to_create)
+    flash(f'Account created successfully! You are now logged in as {user_to_create.username}', category='success')
     return redirect(url_for('market_page'))
    
   if form.errors != {}: #if there are errors from the validations
@@ -49,3 +53,9 @@ def login_page():
       flash(f'Username and password do not match! Please try again', category='danger')
 
   return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET','POST'])
+def logout_page():
+  logout_user()
+  flash("You have been logged out!", category='info')
+  return redirect(url_for('home_page'))
